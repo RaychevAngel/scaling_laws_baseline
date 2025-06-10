1a7fda10617d34ccd3a87c825cba71e4429de91a
angel3481 <angel11drakon@gmail.com>
2025-06-10 02:12:45 +0000

    Updated .gitignore to exclude results/, .sh files, and z.py

--- evaluate_sos.sh
+++
@@ -1,8 +0,0 @@
-for port in 3; do
-  for gpu in 4; do
-    iter=2
-    tmux new-session -d -s "eval_sos_gpu${gpu}_port${port}" \
-      "python -m scripts_sos.sos --iter ${iter} --gpu ${gpu} --port ${port} --mode eval_dev; bash"
-  done
-done
-
